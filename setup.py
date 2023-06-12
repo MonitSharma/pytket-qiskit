ae7f45c0b0cf484d33b7f6a5157ef2630dfa4c8a
Melf <cqc@melf.de>
2023-06-12 15:25:33 +0100

    update pytket

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
     packages=find_namespace_packages(include=["pytket.*"]),
     include_package_data=True,
     install_requires=[
-        "pytket ~= 1.15",
+        "pytket ~= 1.16.0rc2",
         "qiskit ~= 0.43.1",
         "qiskit-ibm-runtime ~= 0.10.0",
         "qiskit-aer ~= 0.12.0",
