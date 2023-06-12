7458a8bd066e900e1414aaf1d9397a086933066f
Melf <cqc@melf.de>
2023-06-12 15:23:06 +0100

    update

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ setup(
     include_package_data=True,
     install_requires=[
         "pytket ~= 1.15",
-        "qiskit ~= 0.42.1",
-        "qiskit-ibm-runtime ~= 0.9.2",
+        "qiskit ~= 0.43.1",
+        "qiskit-ibm-runtime ~= 0.10.0",
         "qiskit-aer ~= 0.12.0",
         "qiskit-ibm-provider ~= 0.5.0",
         "numpy",
